7c81ae5dc0e31c274246902cd22b744fac2a6740
Viacheslav Poturaev <vearutop@gmail.com>
2020-01-24 13:58:13 +0100

    Update dependencies, adopt new features of php-code-builder

--- src/App.php
+++ src/App.php
@@ -7,7 +7,7 @@ use Yaoi\Command\Definition;
 
 class App extends Command\Application
 {
-    public static $ver = 'v1.6.7';
+    public static $ver = 'v1.6.8';
 
     public $diff;
     public $apply;
