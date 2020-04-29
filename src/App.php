0691ca878baa597c03b0b8288aeeb71b2577da96
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2020-04-29 17:31:02 +0200

    Update dependencies (#18)

--- src/App.php
+++ src/App.php
@@ -7,7 +7,7 @@ use Yaoi\Command\Definition;
 
 class App extends Command\Application
 {
-    public static $ver = 'v1.7.8';
+    public static $ver = 'v1.7.9';
 
     public $diff;
     public $apply;
