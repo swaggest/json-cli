15fcc8f32df62f1defae77bbe22c548594ad1edd
Viacheslav Poturaev <vearutop@gmail.com>
2021-04-20 23:45:53 +0200

    Update app version

--- src/App.php
+++ src/App.php
@@ -7,7 +7,7 @@ use Yaoi\Command\Definition;
 
 class App extends Command\Application
 {
-    public static $ver = 'v1.8.5';
+    public static $ver = 'v1.8.6';
 
     public $diff;
     public $apply;
