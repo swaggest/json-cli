da5f24e7903ea5e7487a5bf65f0f5286f64505f7
Luca Di Fazio <highliuk@gmail.com>
2023-11-01 16:38:43 +0100

    Fix #38

--- src/App.php
+++ src/App.php
@@ -7,7 +7,7 @@ use Yaoi\Command\Definition;
 
 class App extends Command\Application
 {
-    public static $ver = 'v1.11.1';
+    public static $ver = 'v1.11.2';
 
     public $diff;
     public $apply;
